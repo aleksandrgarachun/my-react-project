b0f3dd31e5cb9ac5851c33c704f1f89a92bcc6b0
aleksandrgarachun <115419603+aleksandrgarachun@users.noreply.github.com>
2024-03-11 15:22:43 +0300

    new add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,98 @@
 // src/components/App.jsx
 
 
-import { Product } from "./Product";
+import ArticleList from "./ArticleList.jsx";
+import { fetchArticlesWithTopic } from "./articles-api.js";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Article from "./Article.jsx";
+import Loader from "./Loader.jsx";
+import Error from "./Error.jsx";
+import SearchForm from "./SearchForm.jsx";
 
+const App = () => {
+	const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
+  
 
-export default function App() {
+	const handleSearch = async (topic) => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <h1>Best selling</h1>
-
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
+      <SearchForm onSearch={handleSearch} />
+      {loading && <Loader />}
+      {error && <Error />}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
-}
+};
 
+
+
+export default App;
+
+
+/*// src/components/App.jsx
+
+
+import ArticleList from "./ArticleList.jsx";
+import { fetchArticlesWithTopic } from "../articles-api.js";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+				// Встановлюємо стан error в true
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();
+  }, []);
+
+
+  return (
+    <div>
+      <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
+};
+
+
+
+export default App; */
